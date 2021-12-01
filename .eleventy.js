1fbba80c8b21e4229619aeee89b45f072f9df011
Lewis Nyman <hello@lewisnyman.co.uk>
2021-12-01 09:12:47 +0000

    Added logic to auto switch the images

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,17 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("images");
-  };
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addShortcode("day", function() {
+    const now = new Date();
+    const month = now.getUTCMonth();
+    const day = now.getUTCDate();
+    // If it's not december, show the whole map
+    if(month !== 11) {
+      return 23;
+    }
+    // If it's beyond the 23rd, show the whole map
+    if(day > 23) {
+      return 23;
+    }
+    return day;
+  });
+};
